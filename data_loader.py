54a1e5313a75d262a0b5e5d24fa77ddf88f4952c
Anastasiya_UI <stnv.aaa@gmail.com>
2025-09-29 20:55:29 +0300

    Delete data_loader.py

--- data_loader.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-FILE_ID = "1G8DHyYC5oBUepETIWR-xxtZbXcIRsNk5"
-file_url = f"https://drive.google.com/uc?id={FILE_ID}"
-raw_data = pd.read_csv(file_url)  # читаем файл
-print(raw_data.head(10))  # выводим на экран первые 10 строк для проверки
